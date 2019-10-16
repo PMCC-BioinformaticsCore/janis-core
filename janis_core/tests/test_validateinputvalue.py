d23765055471e97549dfa51f1a3f51dd702d7ac4
Michael Franklin <michael.franklin@petermac.org>
2019-10-16 15:43:59 +1100

    Add tests for ValidateInput values

--- janis_core/tests/test_validateinputvalue.py
+++ janis_core/tests/test_validateinputvalue.py
@@ -1,37 +1,43 @@
 import unittest
 
-# from janis_core import Input, String
-#
-#
-# class TestValidateInputvalue(unittest.TestCase):
-#     def test_validate_string_optional_allowoptional_value(self):
-#         v = Input("aa", String(optional=True), "value")
-#         self.assertEqual(True, v.validate_value(True))
-#
-#     def test_validate_string_optional_allowoptional_novalue(self):
-#         v = Input("aa", String(optional=True))
-#         self.assertEqual(True, v.validate_value(True))
-#
-#     def test_validate_string_optional_disallowoptional_value(self):
-#         v = Input("aa", String(optional=True), "value")
-#         self.assertEqual(True, v.validate_value(False))
-#
-#     def test_validate_string_optional_disallowoptional_novalue(self):
-#         v = Input("aa", String(optional=True))
-#         self.assertEqual(True, v.validate_value(False))
-#
-#     def test_validate_string_nooptional_allowoptional_value(self):
-#         v = Input("aa", String(), "value")
-#         self.assertEqual(True, v.validate_value(True))
-#
-#     def test_validate_string_nooptional_allowoptional_novalue(self):
-#         v = Input("aa", String())
-#         self.assertEqual(True, v.validate_value(True))
-#
-#     def test_validate_string_nooptional_disallowoptional_value(self):
-#         v = Input("aa", String(), "value")
-#         self.assertEqual(True, v.validate_value(False))
-#
-#     def test_validate_string_nooptional_disallowoptional_novalue(self):
-#         v = Input("aa", String())
-#         self.assertEqual(False, v.validate_value(False))
+from janis_core import Array, String, Int
+
+
+class TestValidateInputvalue(unittest.TestCase):
+    def test_validate_string_optional_allowoptional_value(self):
+        self.assertTrue(String(optional=True).validate_value("aa", True))
+
+    def test_validate_string_optional_allowoptional_novalue(self):
+        self.assertTrue(String(optional=True).validate_value(None, True))
+
+    def test_validate_string_optional_disallowoptional_value(self):
+        self.assertTrue(String(optional=True).validate_value("aa", False))
+
+    def test_validate_string_optional_disallowoptional_novalue(self):
+        self.assertTrue(String(optional=True).validate_value(None, False))
+
+    def test_validate_string_nooptional_allowoptional_value(self):
+        self.assertTrue(String().validate_value("aa", True))
+
+    def test_validate_string_nooptional_allowoptional_novalue(self):
+        self.assertTrue(String().validate_value(None, True))
+
+    def test_validate_string_nooptional_disallowoptional_value(self):
+        self.assertTrue(String().validate_value("aa", False))
+
+    def test_validate_string_nooptional_disallowoptional_novalue(self):
+        self.assertFalse(String().validate_value(None, False))
+
+    def test_array_valid(self):
+        self.assertTrue(Array(String()).validate_value(["aa", "bb"], True))
+
+    def test_array_valid_optional_internal(self):
+        self.assertTrue(
+            Array(String(optional=True)).validate_value(["aa", None], False)
+        )
+
+    def test_array_invalid_int_string(self):
+        self.assertFalse(Array(String()).validate_value(["aa", 2], True))
+
+    def test_int_invalid_float(self):
+        self.assertFalse(Array(Int()).validate_value(2.0, True))
