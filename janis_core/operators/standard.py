e73dbb2ed71419ac69036ee2415a827bc0984ae9
Michael Franklin <michael.franklin@petermac.org>
2020-04-04 17:16:34 +1100

    Add transform operator

--- janis_core/operators/standard.py
+++ janis_core/operators/standard.py
@@ -1,4 +1,4 @@
-from janis_core.types.common_data_types import String
+from janis_core.types.common_data_types import String, Array, AnyType
 from janis_core.operators.operator import Operator
 
 
@@ -21,3 +21,26 @@ class BasenameOperator(Operator):
 
     def __repr__(self):
         return str(self)
+
+
+class TransformOperator(Operator):
+    def argtypes(self):
+        return [Array(Array(AnyType))]
+
+    def returntype(self):
+        return Array(Array(AnyType))
+
+    def __str__(self):
+        return str(f"transform({self.args[0]})")
+
+    def __repr__(self):
+        return str(self)
+
+    def to_wdl(self, unwrap_operator, *args):
+        return f"transform({unwrap_operator(args[0])})"
+
+    def to_cwl(self, unwrap_operator, *args):
+        return (
+            unwrap_operator(args[0])
+            + ".map(function(c, i) { return q.map(function(row) { return row[i]; }); })"
+        )
