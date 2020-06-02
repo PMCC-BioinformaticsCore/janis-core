3e3b6bed829a979c2955945e0d32460787e59b70
Michael Franklin <michael.franklin@petermac.org>
2020-06-02 12:58:00 +1000

    Add test (which is broken, fix is in molpath branch)

--- janis_core/tests/test_conditionals.py
+++ janis_core/tests/test_conditionals.py
@@ -1,5 +1,6 @@
 import unittest
 
+from janis_core.translations import WdlTranslator
 from janis_core.utils import is_module_available
 from janis_core.workflow.workflow import WorkflowBuilder
 
@@ -32,10 +33,52 @@ class TestConditionals(unittest.TestCase):
         w.input("inp1", str, value="Hello")
         w.input("inp2", str, value="Hi there")
 
-        w.conditional("echoswitch", [(w.inp > 1, Echo(inp=w.inp1)), Echo(inp=w.inp2)])
+        w.conditional(
+            "echoswitch",
+            [
+                (w.inp > 1, Echo(inp=w.inp1)),
+                (w.inp.equals(1), Echo(inp="tasy case")),
+                Echo(inp=w.inp2),
+            ],
+        )
 
         w.output("out", source=w.echoswitch)
 
-        w.translate(
-            "wdl"  # to_disk=True, export_path="~/Desktop/tmp/{name}", validate=True
-        )
+        _, wdl_tools = WdlTranslator.translate_workflow(w)
+        expected = """\
+version development
+
+import "echo_v1_0_0.wdl" as E
+
+workflow echoswitch {
+  input {
+    Int cond_inp
+    String switch_case_1_inp
+    String? switch_case_2_inp = "tasy case"
+    String switch_case_3_inp
+  }
+  if ((cond_inp > 1)) {
+     call E.echo as switch_case_1 {
+      input:
+        inp=switch_case_1_inp
+    }
+  }
+  if (((cond_inp == 1) && !((cond_inp > 1)))) {
+     call E.echo as switch_case_2 {
+      input:
+        inp=select_first([switch_case_2_inp, "tasy case"])
+    }
+  }
+  if (!(((cond_inp > 1) || (cond_inp == 1)))) {
+     call E.echo as switch_case_3 {
+      input:
+        inp=switch_case_3_inp
+    }
+  }
+  output {
+    File out = select_first([switch_case_1.out, switch_case_2.out, switch_case_3.out])
+  }
+}"""
+
+        echoswitch = wdl_tools["echoswitch"].get_string()
+        self.assertEqual(expected, echoswitch)
