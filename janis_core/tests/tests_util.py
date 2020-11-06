f085cdbbb747f761ad08ebb310cfcfb90df32252
Michael Franklin <michael.franklin@petermac.org>
2020-11-06 15:13:55 +1100

    Better WDL basename detection for uniontype

--- janis_core/tests/tests_util.py
+++ janis_core/tests/tests_util.py
@@ -82,8 +82,13 @@ class Test2ItemMethodWrap(TestCase):
         )
 
 
-# class testtranslate(TestCase):
-#     def test1(self):
+class testtranslate(TestCase):
+    def test1(self):
+        from janis_bioinformatics.tools.samtools import SamToolsView_1_9
+
+        SamToolsView_1_9().translate("wdl")
+
+
 # from janis_pipelines import WGSGermlineGATK
 
 # WGSGermlineGATK().translate("cwl")
--- janis_core/translations/wdl.py
+++ janis_core/translations/wdl.py
@@ -552,7 +552,7 @@ EOT"""
                         expression.prefix,
                         inputsdict=inputsdict,
                         string_environment=True,
-                        for_output=True,
+                        for_output=for_output,
                     )
                 }
             )
@@ -1879,7 +1879,9 @@ def translate_input_selector(
     name = resolve_tool_input_value(inp, inputsdict, **debugkwargs)
 
     intype = inp.input_type
-    if selector.remove_file_extension and isinstance(intype, (File, Directory)):
+    if selector.remove_file_extension and (
+        File().can_receive_from(intype) or Directory().can_receive_from(intype)
+    ):
         if isinstance(intype, File) and intype.extension:
             name = f'basename({name}, "{intype.extension}")'
         else:
