6aa81131b70d43330a99a2f8e27d05d8c07839d6
Michael Franklin <michael.franklin@petermac.org>
2021-01-29 10:38:41 +1100

    Add more tests to CWL expression parser

--- janis_core/tests/test_from_cwl.py
+++ janis_core/tests/test_from_cwl.py
@@ -1,25 +1,60 @@
 import unittest
 
-from janis_core import CWlParser
+from janis_core import (
+    CWlParser,
+    InputSelector,
+    BasenameOperator,
+    FileSizeOperator,
+    ReadContents,
+)
 
 
 class TestFromCwlExpressions(unittest.TestCase):
+    parser = CWlParser(cwl_version="v1.0")
+
     def test_number(self):
-        result = CWlParser(cwl_version="v1.0").parse_basic_expression("${return 16 }")
+        expr = "${return 16 }"
+        result = self.parser.parse_basic_expression(expr)
         self.assertEqual(16, result)
 
     def test_number_with_semicolon(self):
-        result = CWlParser(cwl_version="v1.0").parse_basic_expression("${return 16;}")
+        expr = "${return 16;}"
+        result = self.parser.parse_basic_expression(expr)
         self.assertEqual(16, result)
 
     def test_number_with_spaces(self):
-        result = CWlParser(cwl_version="v1.0").parse_basic_expression(
-            "${ return 80000 }"
-        )
+        expr = "${ return 80000 }"
+        result = self.parser.parse_basic_expression(expr)
         self.assertEqual(80000, result)
 
     def test_string(self):
-        result = CWlParser(cwl_version="v1.0").parse_basic_expression(
-            '${ return "over 80000" }'
-        )
+        expr = '${ return "over 80000" }'
+        result = self.parser.parse_basic_expression(expr)
         self.assertEqual("over 80000", result)
+
+    def test_input_selector(self):
+        expr = "$(inputs.my_input)"
+        result = self.parser.parse_basic_expression(expr)
+        self.assertIsInstance(result, InputSelector)
+        self.assertEqual("my_input", result.input_to_select)
+
+    def test_input_selector_with_basename(self):
+        expr = "$(inputs.my_input.basename)"
+        result = self.parser.parse_basic_expression(expr)
+        self.assertIsInstance(result, BasenameOperator)
+        self.assertIsInstance(result.args[0], InputSelector)
+        self.assertEqual("my_input", result.args[0].input_to_select)
+
+    def test_input_selector_with_filesize(self):
+        expr = "$(inputs.my_input.size)"
+        result = self.parser.parse_basic_expression(expr)
+        self.assertIsInstance(result, FileSizeOperator)
+        self.assertIsInstance(result.args[0], InputSelector)
+        self.assertEqual("my_input", result.args[0].input_to_select)
+
+    def test_input_selector_with_contents(self):
+        expr = "$(inputs.my_input.contents)"
+        result = self.parser.parse_basic_expression(expr)
+        self.assertIsInstance(result, ReadContents)
+        self.assertIsInstance(result.args[0], InputSelector)
+        self.assertEqual("my_input", result.args[0].input_to_select)
