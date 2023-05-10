14ac2df91f17efc282f11a9c5c978d911bc457ed
Grace Hall <graceavahall@gmail.com>
2023-05-10 19:21:13 +1000

    secondary files now updated. ToolInput('alignments', BamBai()) -> tuple path(alignments), path(alignments_bai)

--- janis_core/translations/nextflow/naming/process.py
+++ janis_core/translations/nextflow/naming/process.py
@@ -15,22 +15,22 @@ def generic(inp: ToolInput | TInput) -> str:
     
 def secondaries(inp: TInput, duplicate_datatype_exists: bool=False) -> list[str]:
     """returns variable names for each file of secondary array type"""
+    basename = generic(inp)
     dtype: DataType = inp.intype  # type: ignore
     exts = utils.get_extensions(dtype, remove_prefix_symbols=True)
     exts = [x.replace('.', '_') for x in exts]
     
-    # ['bam', 'bai'] -> ['normal_bam', 'normal_bai'] when other tinput 
-    # with same secondary datatype exists
-    if duplicate_datatype_exists:
-        identifier = to_case(inp.id(), case=settings.translate.nextflow.NF_PROCESS_INPUT_CASE)
-        exts = [f'{identifier}_{x}' for x in exts]
-
-    return exts
+    names = [basename] * len(exts)
+    for i, ext in enumerate(exts):
+        if i == 0:
+            continue
+        names[i] = f'{names[i]}_{ext}'
+    return names
 
 def secondaries_array(inp: TInput, duplicate_datatype_exists: bool=False) -> str:
     """returns variable name for secondary array input"""
     if duplicate_datatype_exists:
-        identifier = to_case(inp.id(), case=settings.translate.nextflow.NF_PROCESS_INPUT_CASE)
+        identifier = generic(inp)
         return f'{identifier}_flat'
     else:
         dtype: DataType = inp.intype  # type: ignore
@@ -40,14 +40,14 @@ def secondaries_array(inp: TInput, duplicate_datatype_exists: bool=False) -> str
 
 def file_pair(inp: TInput) -> list[str]:
     """returns variable names for each pair for file pair type"""
-    basename = to_case(inp.id(), case=settings.translate.nextflow.NF_PROCESS_INPUT_CASE)
+    basename = generic(inp)
     pair1 = f'{basename}1'
     pair2 = f'{basename}2'
     return [pair1, pair2]
 
 def file_pair_array(inp: TInput) -> str:
     """returns variable name for file pair array input"""
-    identifier = to_case(inp.id(), case=settings.translate.nextflow.NF_PROCESS_INPUT_CASE)
+    identifier = generic(inp)
     return f'{identifier}_flat'
 
 
