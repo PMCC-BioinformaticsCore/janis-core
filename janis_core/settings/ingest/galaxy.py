9f66623fe5feb315ee2d90be26341251a5a56de0
Grace Hall <graceavahall@gmail.com>
2023-07-07 15:15:51 +1000

    removed unnecessary test file

--- janis_core/settings/ingest/galaxy.py
+++ janis_core/settings/ingest/galaxy.py
@@ -8,10 +8,10 @@ DISABLE_CONTAINER_CACHE = False
 
 JANIS_DATA_DIR = os.path.join(os.getcwd(), '.janis')
 JANIS_INSTALL_DIR = pathlib.Path(__file__).parent.parent.parent.resolve()
-PACKAGE_DATA_DIR = os.path.join(JANIS_INSTALL_DIR, 'ingestion', 'galaxy', 'data')
+GALAXY_DATA_DIR = os.path.join(JANIS_INSTALL_DIR, 'ingestion', 'galaxy', 'data')
 
-GALAXY_CONFIG = f'{PACKAGE_DATA_DIR}/galaxy_config.yaml'
-GALAXY_DATATYPES_YAML = f'{PACKAGE_DATA_DIR}/janis_types.yaml'
+GALAXY_CONFIG = f'{GALAXY_DATA_DIR}/galaxy_config.yaml'
+GALAXY_DATATYPES_YAML = f'{GALAXY_DATA_DIR}/janis_types.yaml'
 CONTAINER_CACHE = f'{JANIS_DATA_DIR}/galaxy_containers/cache.json'
 WRAPPER_CACHE = f'{JANIS_DATA_DIR}/galaxy_wrappers/cache.json'   
 DOWNLOADED_WRAPPERS_DIR = f'{JANIS_DATA_DIR}/galaxy_wrappers'
